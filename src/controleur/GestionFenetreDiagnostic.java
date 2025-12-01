f8cfb127c2575b776f3eefd9f3ce22414891aa4d
Jay Agil Yoogan <jayagilyoogan@gmail.com>
2025-12-01 09:17:41 +0100

    adfa

--- src/controleur/GestionFenetreDiagnostic.java
+++ src/controleur/GestionFenetreDiagnostic.java
@@ -2,27 +2,7 @@ package controleur;
 
 import vue.*;
 
-<<<<<<< HEAD
 public class GestionFenetreDiagnostic extends GestionHeaderEtFooter {
-=======
-import javax.swing.JButton;
-import javax.swing.JMenuItem;
-
-import vue.FenetreAjouterBatiment;
-import vue.FenetreAjouterPaiement;
-import vue.FenetreAssurance;
-import vue.FenetreBienLouable;
-import vue.FenetreCharges;
-import vue.FenetreCompteurs;
-import vue.FenetreContratLocation;
-import vue.FenetreDiagnostic;
-import vue.FenetreLocataire;
-import vue.FenetreLogin;
-import vue.FenetrePaiement;
-import vue.FenetreTravaux;
-
-public class GestionFenetreDiagnostic implements ActionListener {
->>>>>>> 9c8d8cfe3cc112ed5bc1affefc34646795a566c4
 
     private FenetreDiagnostic fenetre;
 
@@ -32,106 +12,15 @@ public class GestionFenetreDiagnostic implements ActionListener {
     }
 
     @Override
-<<<<<<< HEAD
     protected void gererBoutonSpecifique(String texte) {
-
         switch (texte) {
             case "obtenir":
                 fenetre.dispose();
                 break;
         }
-=======
-    public void actionPerformed(ActionEvent e) {
-    	Object source = e.getSource();
-		// JButton
-		if (source instanceof JButton) {
-			JButton btn = (JButton) source;
-	        String texte = btn.getText();
-	        switch (texte) {
-	        case "Retour":
-	            fenetre.dispose();
-	            FenetreBienLouable fenBienLouable = new FenetreBienLouable();
-	            fenBienLouable.setVisible(true);
-	            break;
-	        case "obtenir":
-	            // permet de telecharger la fiche de diagnostique choisi
-	            break;		
-	        }
-	     }
-		
-		// JMenuItem
-		if (source instanceof JMenuItem) {
-			JMenuItem item = (JMenuItem) source;
-	        String texte = item.getText();
-	        switch (texte) {
-	        case "Déconnecter":
-	        	fenetre.dispose();
-	        	FenetreLogin fenLogin = new FenetreLogin();
-	        	fenLogin.setVisible(true);
-	        	break;
-	        case "Ajouter bâtiment" :
-	        	FenetreAjouterBatiment fenAjouterBatiment = new FenetreAjouterBatiment();
-	        	fenetre.getLayeredPane().add(fenAjouterBatiment);
-	        	fenAjouterBatiment.setVisible(true);
-	        	break;
-	        case "Assurance" :
-	        	FenetreAssurance fenAssurance = new FenetreAssurance();
-	        	fenetre.getLayeredPane().add(fenAssurance);
-	        	fenAssurance.setVisible(true);
-	        	break;
-	        case "Compteurs bâtiment" :
-	        	fenetre.dispose();
-	        	FenetreCompteurs fenCompteurBat = new FenetreCompteurs();
-	        	fenCompteurBat.setVisible(true);
-	        	break;
-	        case "Charges bâtiment" :
-	        	fenetre.dispose();
-	        	FenetreCharges fenChargesBat = new FenetreCharges();
-	        	fenChargesBat.setVisible(true);
-	        	break;
-	        case "Contrat location" :
-	        	fenetre.dispose();
-	        	FenetreContratLocation fenContratLocation = new FenetreContratLocation();
-	        	fenContratLocation.setVisible(true);
-	        	break;
-	        case "Compteurs bien louable" :
-	        	fenetre.dispose();
-	        	FenetreCompteurs fenCompteurBL = new FenetreCompteurs();
-	        	fenCompteurBL.setVisible(true);
-	        	break;
-	        case "Travaux" :
-	        	fenetre.dispose();
-	        	FenetreTravaux fenTravaux = new FenetreTravaux();
-	        	fenTravaux.setVisible(true);
-	        	break;
-	        case "Charges bien louable" :
-	        	fenetre.dispose();
-	        	FenetreCharges fenChargesBL = new FenetreCharges();
-	        	fenChargesBL.setVisible(true);
-	        	break;
-	        case "Diagnostics" :
-	        	fenetre.dispose();
-	        	FenetreDiagnostic fenDiagnostic = new FenetreDiagnostic();
-	        	fenDiagnostic.setVisible(true);
-	        	break;
-	        case "Locataires" :
-	        	fenetre.dispose();
-	        	FenetreLocataire fenLocataire = new FenetreLocataire();
-	        	fenLocataire.setVisible(true);
-	        	break;
-	        case "Historique de paiement" :
-	        	fenetre.dispose();
-	        	FenetrePaiement fenPaiement = new FenetrePaiement();
-	        	fenPaiement.setVisible(true);
-	        	break;
-	        case "Ajouter paiement" :
-	        	FenetreAjouterPaiement fenAjouterPaiement = new FenetreAjouterPaiement();
-	        	fenetre.getLayeredPane().add(fenAjouterPaiement);
-	        	fenAjouterPaiement.setVisible(true);
-	        	break;
-	        }
-		}
->>>>>>> 9c8d8cfe3cc112ed5bc1affefc34646795a566c4
     }
 
+    @Override
+    protected void gererMenuSpecifique(String texte) {
+    }
 }
--- src/controleur/GestionFenetrePrincipale.java
+++ src/controleur/GestionFenetrePrincipale.java
@@ -1,180 +1,67 @@
 package controleur;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import vue.*;
 
-import javax.swing.JButton;
-import javax.swing.JMenuItem;
-
-import vue.FenetreAjouterBatiment;
-import vue.FenetreAjouterPaiement;
-import vue.FenetreAssurance;
-import vue.FenetreBienLouable;
-import vue.FenetreCharges;
-import vue.FenetreCompteurs;
-import vue.FenetreContratLocation;
-import vue.FenetreDiagnostic;
-import vue.FenetreLocataire;
-import vue.FenetreLogin;
-import vue.FenetrePaiement;
-import vue.FenetrePrincipale;
-import vue.FenetreTravaux;
-
-public class GestionFenetrePrincipale implements ActionListener, MouseListener {
+public class GestionFenetrePrincipale extends GestionHeaderEtFooter implements MouseListener {
 
     private FenetrePrincipale fenetre;
 
     public GestionFenetrePrincipale(FenetrePrincipale fenetre) {
+        super(fenetre);
         this.fenetre = fenetre;
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-    	Object source = e.getSource();
-		// JButton
-		if (source instanceof JButton) {
-			JButton btn = (JButton) source;
-	        String texte = btn.getText();
-	        switch (texte) {
-	        case "Quitter":
-	            fenetre.dispose();
-	            break;
-	        case "Assurance":
-	            FenetreAssurance assurance = new FenetreAssurance();
-	            fenetre.getLayeredPane().add(assurance);
-	            assurance.setVisible(true);
-	            break;
-	        case "Compteurs":
-	        	fenetre.dispose();
-	        	FenetreCompteurs fenetreCompteurs = new FenetreCompteurs();
-	        	fenetreCompteurs.setVisible(true);
-	            break;
-	        case "Charges":
-	        	fenetre.dispose();
-	            FenetreCharges charges = new FenetreCharges();
-	            charges.setVisible(true);
-	            break;
-	        case "Ajouter":
-	            FenetreAjouterBatiment ajout = new FenetreAjouterBatiment();
-	            fenetre.getLayeredPane().add(ajout);
-	            ajout.setVisible(true);
-	            break;
-	        }
-	     }
-		
-		// JMenuItem
-		if (source instanceof JMenuItem) {
-			JMenuItem item = (JMenuItem) source;
-	        String texte = item.getText();
-	        switch (texte) {
-	        case "Déconnecter":
-	        	fenetre.dispose();
-	        	FenetreLogin fenLogin = new FenetreLogin();
-	        	fenLogin.setVisible(true);
-	        	break;
-	        case "Ajouter bâtiment" :
-	        	FenetreAjouterBatiment fenAjouterBatiment = new FenetreAjouterBatiment();
-	        	fenetre.getLayeredPane().add(fenAjouterBatiment);
-	        	fenAjouterBatiment.setVisible(true);
-	        	break;
-	        case "Assurance" :
-	        	FenetreAssurance fenAssurance = new FenetreAssurance();
-	        	fenetre.getLayeredPane().add(fenAssurance);
-	        	fenAssurance.setVisible(true);
-	        	break;
-	        case "Compteurs bâtiment" :
-	        	fenetre.dispose();
-	        	FenetreCompteurs fenCompteurBat = new FenetreCompteurs();
-	        	fenCompteurBat.setVisible(true);
-	        	break;
-	        case "Charges bâtiment" :
-	        	fenetre.dispose();
-	        	FenetreCharges fenChargesBat = new FenetreCharges();
-	        	fenChargesBat.setVisible(true);
-	        	break;
-	        case "Contrat location" :
-	        	fenetre.dispose();
-	        	FenetreContratLocation fenContratLocation = new FenetreContratLocation();
-	        	fenContratLocation.setVisible(true);
-	        	break;
-	        case "Compteurs bien louable" :
-	        	fenetre.dispose();
-	        	FenetreCompteurs fenCompteurBL = new FenetreCompteurs();
-	        	fenCompteurBL.setVisible(true);
-	        	break;
-	        case "Travaux" :
-	        	fenetre.dispose();
-	        	FenetreTravaux fenTravaux = new FenetreTravaux();
-	        	fenTravaux.setVisible(true);
-	        	break;
-	        case "Charges bien louable" :
-	        	fenetre.dispose();
-	        	FenetreCharges fenChargesBL = new FenetreCharges();
-	        	fenChargesBL.setVisible(true);
-	        	break;
-	        case "Diagnostics" :
-	        	fenetre.dispose();
-	            FenetreDiagnostic fenDiagnostic = new FenetreDiagnostic();
-	            fenDiagnostic.setVisible(true);
-	            break;
-	        case "Locataires" :
-	        	fenetre.dispose();
-	        	FenetreLocataire fenLocataire = new FenetreLocataire();
-	        	fenLocataire.setVisible(true);
-	        	break;
-	        case "Historique de paiement" :
-	        	fenetre.dispose();
-	        	FenetrePaiement fenPaiement = new FenetrePaiement();
-	        	fenPaiement.setVisible(true);
-	        	break;
-	        case "Ajouter paiement" :
-	        	FenetreAjouterPaiement fenAjouterPaiement = new FenetreAjouterPaiement();
-	        	fenetre.getLayeredPane().add(fenAjouterPaiement);
-	        	fenAjouterPaiement.setVisible(true);
-	        	break;
-	        }
-		}
+    protected void gererBoutonSpecifique(String texte) {
+        switch (texte) {
+            case "Quitter":
+                fenetre.dispose();
+                break;
+
+            case "Assurance":
+                FenetreAssurance assurance = new FenetreAssurance();
+                fenetre.getLayeredPane().add(assurance);
+                assurance.setVisible(true);
+                break;
+
+            case "Compteurs":
+                fenetre.dispose();
+                new FenetreCompteurs().setVisible(true);
+                break;
+
+            case "Charges":
+                fenetre.dispose();
+                new FenetreCharges().setVisible(true);
+                break;
+
+            case "Ajouter":
+                FenetreAjouterBatiment ajout = new FenetreAjouterBatiment();
+                fenetre.getLayeredPane().add(ajout);
+                ajout.setVisible(true);
+                break;
+        }
     }
 
+    @Override
+    protected void gererMenuSpecifique(String texte) {
+    }
+
+    @Override
     public void mouseClicked(MouseEvent e) {
         if (e.getClickCount() == 2) {
-        	fenetre.dispose();
+            fenetre.dispose();
             int row = fenetre.getTable().getSelectedRow();
             if (row != -1) {
-                String contrat = (String) fenetre.getTable().getValueAt(row, 0);
-                String date = (String) fenetre.getTable().getValueAt(row, 1);
-                String bien = (String) fenetre.getTable().getValueAt(row, 2);
-                String locataire = (String) fenetre.getTable()
-                    .getValueAt(row, 3);
                 FenetreBienLouable fen = new FenetreBienLouable();
                 fen.setVisible(true);
             }
         }
     }
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override public void mousePressed(MouseEvent e) {}
+    @Override public void mouseReleased(MouseEvent e) {}
+    @Override public void mouseEntered(MouseEvent e) {}
+    @Override public void mouseExited(MouseEvent e) {}
 }
