580244ca2848443dcef1ac67447ca7fe9b655a54
Jay Agil Yoogan <jayagilyoogan@gmail.com>
2025-12-01 16:32:09 +0100

    kljn

--- src/vue/FenetreAjouterBienLouable.java
+++ src/vue/FenetreAjouterBienLouable.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import controleur.GestionFenetreAjouterAssurance;
 import controleur.GestionFenetreAjouterBienLouable;
 
 import javax.swing.JLabel;
