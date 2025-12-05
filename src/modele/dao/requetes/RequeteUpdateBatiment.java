b8fc87b33de096adc7f2450f38d27720dbdfd0b1
lucaviar <luca.andrew.db@gmail.com>
2025-12-05 16:57:15 +0100

    Ajout ReqUpdate Assurance, Batiment, ChargesGenerales

---
+++ src/modele/dao/requetes/RequeteUpdateBatiment.java
@@ -0,0 +1,27 @@
+package modele.dao.requetes;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modele.Batiment;
+
+public class RequeteUpdateBatiment extends Requete<Batiment> {
+    @Override
+    public String requete() {
+        return " Update MSF5131A.SAE_BATIMENT set DATE_CONSTRUCTION =  ? Where ADRESSE = ? ";
+
+    }
+
+    @Override
+    public void parametres(PreparedStatement prSt, String... id)
+        throws SQLException {
+    }
+
+    @Override
+    public void parametres(PreparedStatement prSt, Batiment donnee)
+        throws SQLException {
+        prSt.setString(1, donnee.getDateConstruction().toString());
+        prSt.setString(2, donnee.getAdresse());
+    }
+
+}
