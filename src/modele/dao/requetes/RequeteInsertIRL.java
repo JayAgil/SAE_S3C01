0dfd7333a5a888e0dbd71ec81b6341198b13b9c5
Jay Agil Yoogan <jayagilyoogan@gmail.com>
2025-12-08 16:55:41 +0100

    jhl

---
+++ src/modele/dao/requetes/RequeteInsertIRL.java
@@ -0,0 +1,21 @@
+package modele.dao.requetes;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import modele.IRL;
+
+public class RequeteInsertIRL extends Requete<IRL>{
+
+	
+	public String requete() {
+		// TODO Auto-generated method stub
+		return "INSERT INTO SAE_IRL (Annee, IRL) VALUES (?,?)";
+	}
+	
+	public void parametres(PreparedStatement ps, IRL i) throws SQLException {
+        ps.setLong(1, i.getAnnee());
+        ps.setDouble(2, i.getIRL());
+    }
+
+}
