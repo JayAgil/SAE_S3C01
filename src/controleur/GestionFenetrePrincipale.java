06d7c88f94f5de7d17a4e2ac10b5a584c9a9dbdf
lucaviar <luca.andrew.db@gmail.com>
2025-12-17 10:02:52 +0100

    Fonctionnement du tableau fenPrincipale terminé

--- src/controleur/GestionFenetrePrincipale.java
+++ src/controleur/GestionFenetrePrincipale.java
@@ -1,207 +1,278 @@
 package controleur;
+
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import modele.Batiment;
 import modele.BienLouable;
 import modele.ChargesGenerales;
 import modele.ContratLocation;
 import modele.Locataire;
 import modele.UtOracleDataSource;
+import modele.dao.DaoBatiment;
 import modele.dao.DaoBienLouable;
 import modele.dao.DaoChargesGenerales;
 import modele.dao.DaoContratLocation;
 import modele.dao.DaoLocataire;
-import vue.*;
+import vue.FenetreAjouterBatiment;
+import vue.FenetreAssurance;
+import vue.FenetreBienLouable;
+import vue.FenetreCharges;
+import vue.FenetreCompteurs;
+import vue.FenetreContratLocation;
+import vue.FenetreLocataire;
+import vue.FenetrePaiement;
+import vue.FenetrePrincipale;
 
-public class GestionFenetrePrincipale extends GestionHeaderEtFooter implements MouseListener{
+public class GestionFenetrePrincipale extends GestionHeaderEtFooter
+    implements MouseListener {
 
     private FenetrePrincipale fenetre;
-	private DaoBienLouable daoBienLouable;
+    private DaoBienLouable daoBienLouable;
 
-    public GestionFenetrePrincipale(FenetrePrincipale fenetre){
+    public GestionFenetrePrincipale(FenetrePrincipale fenetre) {
         super(fenetre);
-        this.fenetre = fenetre;        
+        this.fenetre = fenetre;
     }
-    
-    public List<ChargesGenerales> getDonneesChargesGenerale() throws SQLException {
+
+    public List<ChargesGenerales> getDonneesChargesGenerale()
+        throws SQLException {
         DaoChargesGenerales dao = new DaoChargesGenerales();
         String[] id = new String[1];
         id[0] = this.fenetre.getChosenBatiment();
         return dao.findByIdBatiment(id);
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source instanceof JComboBox) {
+            fenetre.getTableBienLouable().clearSelection();
+            viderTable();
+            RemplirTableau();
+        } else {
+            super.actionPerformed(e);
+        }
+    }
+
+    private void viderTable() {
+        DefaultTableModel model = (DefaultTableModel) fenetre
+            .getTableBienLouable()
+            .getModel();
+        model.setRowCount(0);
+    }
+
     @Override
     protected void gererBoutonSpecifique(String texte) throws SQLException {
         switch (texte) {
-            case "Quitter":
-                fenetre.dispose();
-                UtOracleDataSource.Deconnecter();
-                break;
+        case "Quitter":
+            fenetre.dispose();
+            UtOracleDataSource.Deconnecter();
+            break;
 
-            case "Assurance":
-                FenetreAssurance assurance = new FenetreAssurance();
-                assurance.setVisible(true);
-                fenetre.dispose();            
-                break;
+        case "Assurance":
+            FenetreAssurance assurance = new FenetreAssurance();
+            assurance.setVisible(true);
+            fenetre.dispose();
+            break;
 
-            case "Compteurs":
-                new FenetreCompteurs("FenetrePrincipale").setVisible(true);
-                fenetre.dispose();
-                break;
+        case "Compteurs":
+            new FenetreCompteurs("FenetrePrincipale").setVisible(true);
+            fenetre.dispose();
+            break;
 
-            case "Charges":
-                new FenetreCharges("FenetrePrincipale", getDonneesChargesGenerale()).setVisible(true);
-                fenetre.dispose();
-                break;
+        case "Charges":
+            new FenetreCharges("FenetrePrincipale", getDonneesChargesGenerale())
+                .setVisible(true);
+            fenetre.dispose();
+            break;
 
-            case "Ajouter":
-                FenetreAjouterBatiment ajout = new FenetreAjouterBatiment();
-                fenetre.getLayeredPane().add(ajout);
-                ajout.setVisible(true);
-                break;
-            case "Importer Un Fichier CSV":
-            	JFileChooser chooser = new JFileChooser();
-                int result = chooser.showOpenDialog(null);
+        case "Ajouter":
+            FenetreAjouterBatiment ajout = new FenetreAjouterBatiment();
+            fenetre.getLayeredPane().add(ajout);
+            ajout.setVisible(true);
+            break;
+        case "Importer Un Fichier CSV":
+            JFileChooser chooser = new JFileChooser();
+            int result = chooser.showOpenDialog(null);
 
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File selectedFile = chooser.getSelectedFile();
-                    System.out.println("Selected file: " + selectedFile.getAbsolutePath());
-                    mAJDeBaseDeDonnees(selectedFile);
-                }
-                break;
-            case "Généré":
-            	try {
-        			this.daoBienLouable = new DaoBienLouable();
-        		} catch (SQLException e) {
-        			e.printStackTrace();
-        		}
-                List<BienLouable> listBienLouable;
-                try {
-                    listBienLouable = daoBienLouable.findAll();
-                    for (int i = 0; i < listBienLouable.size(); i++) {
-                    	DaoContratLocation daoContrat = new DaoContratLocation();
-                    	DaoLocataire daoLocataire = new DaoLocataire();
-                        List<ContratLocation> listContrat = daoContrat.findByBienLouable(listBienLouable.get(i).getIdBienLouable());
-                        if (listContrat != null) {
-                        	//List<Locataire> listLocataire = daoLocataire.findLocataireByContrat(listContrat.get(0).getNumeroDeContrat());
-                        	//System.out.println(listLocataire);
-                        	EcrireLigneTableBienLouable(i, listBienLouable.get(i), listContrat.get(0));
-                        }
-                    }
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
-                break;
-             }
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = chooser.getSelectedFile();
+                System.out.println(
+                    "Selected file: " + selectedFile.getAbsolutePath());
+                mAJDeBaseDeDonnees(selectedFile);
+            }
+            break;
         }
-    
-        
-    private void mAJDeBaseDeDonnees(File file) {
-    	return;
     }
 
+    private void mAJDeBaseDeDonnees(File file) {
+        return;
+    }
 
     @Override
     protected void gererMenuSpecifique(String texte) {
     }
 
     @Override
-    public void mouseClicked(MouseEvent e){
-    	if (e.getClickCount() == 2 && e.getSource() instanceof JTable) {
-    	    JTable table = (JTable) e.getSource();
-    	    int row = table.rowAtPoint(e.getPoint());
-    	    int column = table.columnAtPoint(e.getPoint()); 
-    	    int targetColumn = 2;
-    	    if (row != -1 && column == targetColumn) {
-    	        fenetre.dispose();
-    	        FenetreBienLouable fen;
-				try {
-					String idBien = table.getValueAt(row, column).toString();
-					DaoBienLouable daoBL = new DaoBienLouable();
-					BienLouable bien = daoBL.findById(idBien);
-					// pass the bien selected by the user here i have put here null but there must be a bien that the user clicked
-					fen = new FenetreBienLouable("FenetrePrincipale", bien);
-	    	        fen.setVisible(true);
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-    	    }
-    	}
-    	
-    	if (e.getClickCount() == 2 && e.getSource() instanceof JTable) {
-    	    JTable table = (JTable) e.getSource();
-    	    int row = table.rowAtPoint(e.getPoint());
-    	    int column = table.columnAtPoint(e.getPoint()); 
-    	    int targetColumn = 3;
-    	    if (row != -1 && column == targetColumn) {
-    	        fenetre.dispose();
-    	        FenetreLocataire fen = new FenetreLocataire("FenetrePrincipale", null);
-    	        fen.setVisible(true);
-    	    }
-    	}
+    public void mouseClicked(MouseEvent e) {
+        if (e.getClickCount() == 2 && e.getSource() instanceof JTable) {
+            JTable table = (JTable) e.getSource();
+            int row = table.rowAtPoint(e.getPoint());
+            int column = table.columnAtPoint(e.getPoint());
+            int targetColumn = 2;
+            if (row != -1 && column == targetColumn) {
+                fenetre.dispose();
+                FenetreBienLouable fen;
+                try {
+                    String idBien = table.getValueAt(row, column).toString();
+                    DaoBienLouable daoBL = new DaoBienLouable();
+                    BienLouable bien = daoBL.findById(idBien);
+                    // pass the bien selected by the user here i have put here
+                    // null but there must be a bien that the user clicked
+                    fen = new FenetreBienLouable("FenetrePrincipale", bien);
+                    fen.setVisible(true);
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
 
-        if(e.getSource() == fenetre.getPanelRevenu()) {
-        	FenetreContratLocation fCL;
+        if (e.getClickCount() == 2 && e.getSource() instanceof JTable) {
+            JTable table = (JTable) e.getSource();
+            int row = table.rowAtPoint(e.getPoint());
+            int column = table.columnAtPoint(e.getPoint());
+            int targetColumn = 3;
+            if (row != -1 && column == targetColumn) {
+                fenetre.dispose();
+                FenetreLocataire fen = new FenetreLocataire("FenetrePrincipale",
+                    null);
+                fen.setVisible(true);
+            }
+        }
+
+        if (e.getSource() == fenetre.getPanelRevenu()) {
+            FenetreContratLocation fCL;
 			try {
-				fCL = new FenetreContratLocation("FenPrincipale", null);
-	        	fCL.setVisible(true);
+				fCL = new FenetreContratLocation(
+				    "FenPrincipale", null);
+	            fCL.setVisible(true);
 			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
         }
-        if(e.getSource() == fenetre.getPanelNbLoyerPasPaye()) {
-        	FenetreBienLouable fBL;
+        if (e.getSource() == fenetre.getPanelNbLoyerPasPaye()) {
+            FenetreBienLouable fBL;
+            try {
+                fBL = new FenetreBienLouable("FenetrePrincipale", null);
+                fBL.setVisible(true);
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+        if (e.getSource() == fenetre.getPanelRevenu_1()) {
+            FenetrePaiement fp = new FenetrePaiement(null);
+            fp.setVisible(true);
+        }
+        if (e.getSource() == fenetre.getPanelNbLoyePasPaye_1()) {
+            FenetreContratLocation fCL;
 			try {
-				fBL = new FenetreBienLouable("FenetrePrincipale", null);
-	        	fBL.setVisible(true);
+				fCL = new FenetreContratLocation(
+				    "FenPrincipale", null);
+				fCL.setVisible(true);
 			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
         }
-        if(e.getSource() == fenetre.getPanelRevenu_1()) {
-        	FenetrePaiement fp = new FenetrePaiement(null);
-        	fp.setVisible(true);
-        }
-        if(e.getSource() == fenetre.getPanelNbLoyePasPaye_1()) {
-        	FenetreContratLocation fCL;
-			try {
-				fCL = new FenetreContratLocation("FenPrincipale", null);
-	        	fCL.setVisible(true);
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-        }
-        
+
     }
-    
-    public void EcrireLigneTableBienLouable(int numeroLigne,
-            BienLouable bienLouable, ContratLocation contrat) throws SQLException {
-            JTable table = fenetre.getTableBienLouable();
-            DefaultTableModel model = (DefaultTableModel) table.getModel();
-            if (model.getColumnCount() == 0) {
-                model.setColumnIdentifiers(new String[] { "Contrat Location",
-                    "Nombre de piece", "Bien Louable", "Locataire Référent" });
-            }
-        	if (model.getRowCount() == numeroLigne) {
-                model.addRow(new Object[] { null, null, null, null });
-            }
-            model.setValueAt(contrat.getNumeroDeContrat(), numeroLigne, 0);
-            model.setValueAt(bienLouable.getNbPieces(), numeroLigne, 1);
-            model.setValueAt(bienLouable.getTypeBienLouable(), numeroLigne, 2);
-            model.setValueAt("Pablo", numeroLigne, 3);
-            
-        }
 
-    @Override public void mousePressed(MouseEvent e) {}
-    @Override public void mouseReleased(MouseEvent e) {}
-    @Override public void mouseEntered(MouseEvent e) {}
-    @Override public void mouseExited(MouseEvent e) {}
+    public void remplirComboBatiment() {
+        try {
+            DaoBatiment daoBatiment = new DaoBatiment();
+            List<Batiment> batiments = daoBatiment.findAll();
+
+            JComboBox<String> combo = fenetre.getCbBatiment();
+            combo.removeAllItems();
+
+            for (Batiment b : batiments) {
+                combo.addItem(b.getAdresse());
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void RemplirTableau() {
+        try {
+            daoBienLouable = new DaoBienLouable();
+            String batiment = fenetre.getChosenBatiment();
+
+            List<BienLouable> listBienLouable = daoBienLouable
+                .findByBatiment(batiment);
+
+            for (int i = 0; i < listBienLouable.size(); i++) {
+                DaoContratLocation daoContrat = new DaoContratLocation();
+                List<ContratLocation> listContrat = daoContrat
+                    .findByBienLouable(
+                        listBienLouable.get(i).getIdBienLouable());
+
+                if (listContrat != null && !listContrat.isEmpty()) {
+                    EcrireLigneTableBienLouable(i, listBienLouable.get(i),
+                        listContrat.get(0));
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void EcrireLigneTableBienLouable(int numeroLigne,
+        BienLouable bienLouable, ContratLocation contrat) throws SQLException {
+        DaoLocataire daoLocataire = new DaoLocataire();
+        List<Locataire> listLocataire = daoLocataire
+            .findLocataireByContrat(contrat.getNumeroDeContrat());
+        JTable table = fenetre.getTableBienLouable();
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        if (model.getColumnCount() == 0) {
+            model.setColumnIdentifiers(new String[] { "Contrat Location",
+                "Nombre de piece", "Bien Louable", "Locataire Référent" });
+        }
+        if (model.getRowCount() == numeroLigne) {
+            model.addRow(new Object[] { null, null, null, null });
+        }
+        model.setValueAt(contrat.getNumeroDeContrat(), numeroLigne, 0);
+        model.setValueAt(bienLouable.getNbPieces(), numeroLigne, 1);
+        model.setValueAt(bienLouable.getTypeBienLouable(), numeroLigne, 2);
+        model.setValueAt(listLocataire.get(0).getNomComplet(), numeroLigne, 3);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
 }
--- src/modele/dao/DaoBienLouable.java
+++ src/modele/dao/DaoBienLouable.java
@@ -1,65 +1,77 @@
 package modele.dao;
+
 import java.sql.ResultSet;
-import modele.dao.requetes.*;
 import java.sql.SQLException;
 import java.util.List;
 
 import modele.Batiment;
 import modele.BienLouable;
+import modele.dao.requetes.RequeteDeleteBienLouable;
+import modele.dao.requetes.RequeteInsertBienLouable;
+import modele.dao.requetes.RequeteSelectBienLouable;
+import modele.dao.requetes.RequeteSelectBienLouableByBat;
+import modele.dao.requetes.RequeteSelectBienLouableById;
+import modele.dao.requetes.RequeteUpdateBienLouable;
 
-public class DaoBienLouable extends DaoModele<BienLouable> implements Dao<BienLouable> {
+public class DaoBienLouable extends DaoModele<BienLouable>
+    implements Dao<BienLouable> {
 
-	public DaoBienLouable() throws SQLException {
-		super();
-	}
+    public DaoBienLouable() throws SQLException {
+        super();
+    }
 
-	@Override
-	public int create(BienLouable t) throws SQLException {
-		return miseAJour(new RequeteInsertBienLouable(), t);
-		
-	}
+    @Override
+    public int create(BienLouable t) throws SQLException {
+        return miseAJour(new RequeteInsertBienLouable(), t);
 
-	@Override
-	public int update(BienLouable t) throws SQLException {
-		return miseAJour(new RequeteUpdateBienLouable(), t);
-		
-	}
+    }
 
-	@Override
-	public int delete(BienLouable t) throws SQLException {
-		return miseAJour(new RequeteDeleteBienLouable(), t);
-	}
+    @Override
+    public int update(BienLouable t) throws SQLException {
+        return miseAJour(new RequeteUpdateBienLouable(), t);
 
-	@Override
-	public BienLouable findById(String... id) throws SQLException {
-		return this.findById(new RequeteSelectBienLouableById(), id);
-	}
+    }
 
-	@Override
-	public List<BienLouable> findAll() throws SQLException {
-		return this.find(new RequeteSelectBienLouable());
-	}
-	
-	public List<BienLouable> findByIdBat(String... id) throws SQLException {
-		return this.find(new RequeteSelectBienLouableByBat(), id);
-	}
+    @Override
+    public int delete(BienLouable t) throws SQLException {
+        return miseAJour(new RequeteDeleteBienLouable(), t);
+    }
 
-	@Override
-	protected BienLouable creerInstance(ResultSet curseur) throws SQLException {
-		String id_bienLouable = curseur.getString(1);
-		String numFiscale = curseur.getString(2);
-		String adresse = curseur.getString(3);
-		double surface = curseur.getDouble(4);
-		int nbPieces = curseur.getInt(5);
-		String typeBienLouable = curseur.getString(6);
-		String a = curseur.getString(8);
-		DaoBatiment daoBat = new DaoBatiment();
-		Batiment b = daoBat.findById(a);
-		String id = curseur.getString(7);
-		DaoBienLouable daoBl = new DaoBienLouable();
-		BienLouable bl = daoBl.findById(id);
-		
-		return new BienLouable(id_bienLouable,numFiscale,adresse,surface,nbPieces,typeBienLouable, b,bl );
-	}
+    @Override
+    public BienLouable findById(String... id) throws SQLException {
+        return this.findById(new RequeteSelectBienLouableById(), id);
+    }
+
+    @Override
+    public List<BienLouable> findAll() throws SQLException {
+        return this.find(new RequeteSelectBienLouable());
+    }
+
+    public List<BienLouable> findByBatiment(String... id) throws SQLException {
+        return this.find(new RequeteSelectBienLouableByBat(), id);
+    }
+
+    public List<BienLouable> findByIdBat(String... id) throws SQLException {
+        return this.find(new RequeteSelectBienLouableByBat(), id);
+    }
+
+    @Override
+    protected BienLouable creerInstance(ResultSet curseur) throws SQLException {
+        String id_bienLouable = curseur.getString(1);
+        String numFiscale = curseur.getString(2);
+        String adresse = curseur.getString(3);
+        double surface = curseur.getDouble(4);
+        int nbPieces = curseur.getInt(5);
+        String typeBienLouable = curseur.getString(6);
+        String a = curseur.getString(8);
+        DaoBatiment daoBat = new DaoBatiment();
+        Batiment b = daoBat.findById(a);
+        String id = curseur.getString(7);
+        DaoBienLouable daoBl = new DaoBienLouable();
+        BienLouable bl = daoBl.findById(id);
+
+        return new BienLouable(id_bienLouable, numFiscale, adresse, surface,
+            nbPieces, typeBienLouable, b, bl);
+    }
 
 }
