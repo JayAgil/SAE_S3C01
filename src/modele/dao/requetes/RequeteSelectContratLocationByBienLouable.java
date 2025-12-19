3d6c5722fe96675c1a6ca4ddcfcb5decdcf95d12
Jay Agil Yoogan <jayagilyoogan@gmail.com>
2025-12-19 14:49:46 +0100

    fadf

--- src/modele/dao/requetes/RequeteSelectContratLocationByBienLouable.java
+++ src/modele/dao/requetes/RequeteSelectContratLocationByBienLouable.java
@@ -9,7 +9,7 @@ public class RequeteSelectContratLocationByBienLouable
     extends Requete<ContratLocation> {
     @Override
     public String requete() {
-        return " Select * from MSF5131A.MSF5131A.SAE_ContratLocation where FK_ID_BIENLOUABLE = ? ";
+        return " Select * from MSF5131A.SAE_ContratLocation where FK_ID_BIENLOUABLE = ? ";
     }
 
     @Override
