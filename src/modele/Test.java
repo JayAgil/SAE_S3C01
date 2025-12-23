b6f89c1a2562fb31a0adeb78fe7fe74da96ee23f
koshuanair <koshua.nair@etu.iut-tlse3.fr>
2025-12-23 19:48:30 +0100

    ft

--- src/modele/Test.java
+++ src/modele/Test.java
@@ -5,11 +5,20 @@ import java.sql.SQLException;
 import modele.dao.DaoBienLouable;
 
 public class Test {
+<<<<<<< Updated upstream
 	public static void main(String[] args) throws SQLException {
 	    UtOracleDataSource.CreerAcces("NRK4981A", "abcd123");
 	    DaoBienLouable dbl = new DaoBienLouable();
 	    for (BienLouable b : dbl.findAll()) {
 	    	System.out.println(b.toString());
 	    }
+=======
+
+	public static void main(String[] args) throws SQLException {
+		DaoBienLouable dbl = new DaoBienLouable();
+		for (BienLouable b : dbl.findAll()) {
+			System.out.println(b.toString());
+		}
+>>>>>>> Stashed changes
 	}
 }
