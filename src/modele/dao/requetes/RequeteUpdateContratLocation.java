61179956bafcba660ad7ce556f063d0ff6068997
aneesafaisal <aneesa.amdfaisal@icloud.com>
2026-01-05 15:31:42 +0100

    up

--- src/modele/dao/requetes/RequeteUpdateContratLocation.java
+++ src/modele/dao/requetes/RequeteUpdateContratLocation.java
@@ -2,31 +2,30 @@ package modele.dao.requetes;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import modele.ContratLocation;
 
 public class RequeteUpdateContratLocation extends Requete<ContratLocation> {
-	
-	@Override
-    public String requete() {
-        return "Update MSF5131A.SAE_ContratLocation "
-        		+ "set Date_debut = ?,"
-        		+ "Date_Fin = ?, Montant_de_caution = ?,"
-        		+ "Provision_Charge = ?, Solde = ?,"
-        		+ "Montant_Mensuel = ?, Date_versement = ?,"
-        		+ "Index_Compteur_Eau  = ?, Index_Compteur_Electrcite = ?,"
-        		+ "Index_Compteur_Gaz = ?"
-        		+ "Where Numero_de_contrat = ?";
-    }
 
     @Override
-    public void parametres(PreparedStatement prSt, String... id)
-        throws SQLException {
+    public String requete() {
+        return "UPDATE MSF5131A.SAE_ContratLocation SET "
+             + "Date_debut = ?, "
+             + "Date_Fin = ?, "
+             + "Montant_de_caution = ?, "
+             + "Provision_Charge = ?, "
+             + "Solde = ?, "
+             + "Montant_Mensuel = ?, "
+             + "Date_versement = ?, "
+             + "Index_Compteur_Eau = ?, "
+             + "Index_Compteur_Electricite = ?, "
+             + "Index_Compteur_Gaz = ? "
+             + "WHERE Numero_de_contrat = ?";
     }
 
     @Override
     public void parametres(PreparedStatement prSt, ContratLocation donnee)
-        throws SQLException {
+            throws SQLException {
+
         prSt.setDate(1, donnee.getDateDebut());
         prSt.setDate(2, donnee.getDateFin());
         prSt.setDouble(3, donnee.getMontantDeCaution());
@@ -37,7 +36,6 @@ public class RequeteUpdateContratLocation extends Requete<ContratLocation> {
         prSt.setDouble(8, donnee.getIndexCompteurEau());
         prSt.setDouble(9, donnee.getIndexCompteurElectricite());
         prSt.setDouble(10, donnee.getIndexCompteurGaz());
+        prSt.setString(11, donnee.getNumeroDeContrat());
     }
-
-
 }
