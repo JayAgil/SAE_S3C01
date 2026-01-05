171de923425d86767c16a66ad91ce9fdbd470c99
aneesafaisal <aneesa.amdfaisal@icloud.com>
2026-01-05 12:04:24 +0100

    up

--- src/vue/FenetreFacture.java
+++ src/vue/FenetreFacture.java
@@ -2,11 +2,15 @@ package vue;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import modele.Entreprise;
 import modele.Facture;
 
 public class FenetreFacture extends JFrame {
@@ -15,15 +19,34 @@ public class FenetreFacture extends JFrame {
 
     public FenetreFacture(Facture facture) {
 
+        Entreprise entreprise = facture.getEntreprise();
+
         setTitle("Facture " + facture.getNumeroFacture());
         setSize(600, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        JTextArea area = new JTextArea(facture.getTexteFacture());
-        area.setEditable(false);
-        area.setFont(new Font("Monospaced", Font.PLAIN, 13));
+        JPanel panelEntreprise = new JPanel(new GridLayout(4, 1, 5, 5));
+        panelEntreprise.setBorder(BorderFactory.createTitledBorder("Entreprise"));
 
-        add(new JScrollPane(area), BorderLayout.CENTER);
+        panelEntreprise.add(createField("SIRET", entreprise.getNumSiret()));
+        panelEntreprise.add(createField("Nom", entreprise.getNom()));
+        panelEntreprise.add(createField("Adresse", entreprise.getAdresse()));
+        panelEntreprise.add(createField("Spécialité", entreprise.getSpecialite()));
+
+        JTextArea areaFacture = new JTextArea(facture.getTexteFacture());
+        areaFacture.setEditable(false);
+        areaFacture.setFont(new Font("Monospaced", Font.PLAIN, 13));
+
+        add(panelEntreprise, BorderLayout.NORTH);
+        add(new JScrollPane(areaFacture), BorderLayout.CENTER);
+    }
+
+    private JTextArea createField(String label, String value) {
+        JTextArea area = new JTextArea(label + " : " + value);
+        area.setEditable(false);
+        area.setOpaque(false);
+        area.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        return area;
     }
 }
