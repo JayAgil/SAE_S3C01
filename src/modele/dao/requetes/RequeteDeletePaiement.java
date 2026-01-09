a582f489592010400acb00617edd2d9829c4b346
Jay Agil Yoogan <jayagilyoogan@gmail.com>
2026-01-09 14:55:48 +0100

    adc

--- src/modele/dao/requetes/RequeteDeletePaiement.java
+++ src/modele/dao/requetes/RequeteDeletePaiement.java
@@ -12,7 +12,6 @@ public class RequeteDeletePaiement extends Requete<Paiement>{
 		return "DELETE FROM MSF5131A.SAE_PAIEMENT where Id_Paiement = ? ";
 	}
 	
-	
 	@Override
 	public void parametres(PreparedStatement ps, Paiement p) throws SQLException {
 		ps.setString(1, p.getId_paiement());
