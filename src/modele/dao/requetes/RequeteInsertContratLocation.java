ed4fc74191e5f52b61f337b01eed242c96864b81
Jay Agil Yoogan <jayagilyoogan@gmail.com>
2026-01-09 17:02:40 +0100

    daf

--- src/modele/dao/requetes/RequeteInsertContratLocation.java
+++ src/modele/dao/requetes/RequeteInsertContratLocation.java
@@ -13,18 +13,27 @@ public class RequeteInsertContratLocation extends Requete<ContratLocation> {
 	}
 	
 	@Override
-    public void parametres(PreparedStatement ps, ContratLocation c) throws SQLException {
-        ps.setString(1, c.getNumeroDeContrat());
-        ps.setDate(2, c.getDateDebut());
-        ps.setDate(3, c.getDateFin());
-        ps.setDouble(4, c.getMontantDeCaution());
-        ps.setDouble(5, c.getProvisionCharge());
-        ps.setDouble(6, c.getSolde());
-        ps.setDouble(7, c.getMontantMensuel());
-        ps.setDate(8, c.getDateVersement());
-        ps.setDouble(9, c.getIndexCompteurEau());
-        ps.setDouble(10, c.getIndexCompteurElectricite());
-        ps.setDouble(11, c.getIndexCompteurGaz());
-        ps.setString(12, c.getBienLouable().getIdBienLouable());
-    }
+	public void parametres(PreparedStatement ps, ContratLocation c) throws SQLException {
+	    ps.setString(1, c.getNumeroDeContrat());
+
+	    if (c.getDateDebut() != null) ps.setDate(2, c.getDateDebut());
+	    else ps.setNull(2, java.sql.Types.DATE);
+
+	    if (c.getDateFin() != null) ps.setDate(3, c.getDateFin());
+	    else ps.setNull(3, java.sql.Types.DATE);
+
+	    ps.setDouble(4, c.getMontantDeCaution());
+	    ps.setDouble(5, c.getProvisionCharge());
+	    ps.setDouble(6, c.getSolde());
+	    ps.setDouble(7, c.getMontantMensuel());
+
+	    if (c.getDateVersement() != null) ps.setDate(8, c.getDateVersement());
+	    else ps.setNull(8, java.sql.Types.DATE);
+
+	    ps.setDouble(9, c.getIndexCompteurEau());
+	    ps.setDouble(10, c.getIndexCompteurElectricite());
+	    ps.setDouble(11, c.getIndexCompteurGaz());
+	    ps.setString(12, c.getBienLouable().getIdBienLouable());
+	}
+
 }
