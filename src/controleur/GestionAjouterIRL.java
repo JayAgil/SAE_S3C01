55a9067065e3b54af5bbddb405dfb745e6395985
aneesafaisal <aneesa.amdfaisal@icloud.com>
2026-01-12 11:56:43 +0100

    up

--- src/controleur/GestionAjouterIRL.java
+++ src/controleur/GestionAjouterIRL.java
@@ -19,26 +19,42 @@ public class GestionAjouterIRL extends GestionButtonFenetreAjouter {
 		this.fenetreAjouterIRL = fenetreAjouterIRL;
 	}
 
+	/**
+	 * Récupère tous les JTextFields de la fenêtre pour lire les données saisies.
+	 * 
+	 * @return liste des champs texte
+	 */
 	@Override
 	protected List<JTextField> getTextFields() {
 		return fenetreAjouterIRL.getAllTextFields();
 	}
 
+	/**
+	 * Récupère la fenêtre interne associée.
+	 * 
+	 * @return JInternalFrame correspondant à fenetreAjouterIRL
+	 */
 	@Override
 	protected JInternalFrame getFrame() {
 		return this.fenetreAjouterIRL;
 	}
 
+	/**
+	 * Gère l'action de création de l'IRL. - Vérifie la validité des données saisies
+	 * - Crée un objet IRL - Tente l'insertion en base via DaoIRL - Affiche un
+	 * message de succès ou d'erreur
+	 */
 	@Override
 	protected void gererAction() {
 		try {
 			DaoIRL dao = new DaoIRL();
 			List<JTextField> donnees = this.getTextFields();
-			if(Double.parseDouble(donnees.get(1).getText())>4 || Double.parseDouble(donnees.get(1).getText())<0) {
+			if (Double.parseDouble(donnees.get(1).getText()) > 4 || Double.parseDouble(donnees.get(1).getText()) < 0) {
 				JOptionPane.showMessageDialog(null, "La valeur de trimestre doit être compris entre 1 et 4 !", "Erreur",
 						JOptionPane.ERROR_MESSAGE);
 			}
-			IRL irl = new IRL(Integer.parseInt(donnees.get(0).getText()), Integer.parseInt(donnees.get(1).getText()), Double.parseDouble(donnees.get(2).getText()));
+			IRL irl = new IRL(Integer.parseInt(donnees.get(0).getText()), Integer.parseInt(donnees.get(1).getText()),
+					Double.parseDouble(donnees.get(2).getText()));
 			if (dao.create(irl) == 1) {
 				JOptionPane.showMessageDialog(null, "IRL ajoutée avec succès !", "Succès",
 						JOptionPane.INFORMATION_MESSAGE);
@@ -46,26 +62,17 @@ public class GestionAjouterIRL extends GestionButtonFenetreAjouter {
 			} else {
 				JOptionPane.showMessageDialog(null, "Échec de l'ajout de l'IRL.", "Erreur", JOptionPane.ERROR_MESSAGE);
 			}
-			
+
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 
-			if (e.getErrorCode() == 1) { 
-		        JOptionPane.showMessageDialog(
-		            null,
-		            "Cet IRL existe déjà (clé primaire).",
-		            "Doublon",
-		            JOptionPane.WARNING_MESSAGE
-		        );
-		    } else {
-		        JOptionPane.showMessageDialog(
-		            null,
-		            "Erreur SQL : " + e.getMessage(),
-		            "Erreur base de données",
-		            JOptionPane.ERROR_MESSAGE
-		        );
-		    }
+			if (e.getErrorCode() == 1) {
+				JOptionPane.showMessageDialog(null, "Cet IRL existe déjà (clé primaire).", "Doublon",
+						JOptionPane.WARNING_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null, "Erreur SQL : " + e.getMessage(), "Erreur base de données",
+						JOptionPane.ERROR_MESSAGE);
+			}
 		}
 	}
 
