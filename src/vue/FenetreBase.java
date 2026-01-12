d8c4a74ef6b77b8dbfae3719e6411c4d35ced5c3
aneesafaisal <aneesa.amdfaisal@icloud.com>
2026-01-12 09:55:18 +0100

    up

--- src/vue/FenetreBase.java
+++ src/vue/FenetreBase.java
@@ -10,41 +10,36 @@ public abstract class FenetreBase extends JFrame {
 	public JMenu mnBatiment;
 	public JMenu mnBienLouable;
 	public JMenu mnPaiement;
-
-	// ===== MENU ITEMS =====
 	public JMenuItem mntmDeconnecter;
 	public JMenuItem mntmAjouterBat;
 	public JMenuItem mntmCompteur;
 	public JMenuItem mntmCharge;
-
 	public JMenuItem mntmContratLocation;
 	public JMenuItem mntmTravaux;
 	public JMenuItem mntmLocataire;
-
 	public JMenuItem mntmHistorique;
 	public JMenuItem mntmAjout;
 
 	public FenetreBase() {
 
-		// Menus
 		mnProfil = new JMenu("Profil");
 		mnBatiment = new JMenu("Batiment");
 		mnBienLouable = new JMenu("Bien louable");
 		mnPaiement = new JMenu("Paiement");
-
-		// Menu items
 		mntmDeconnecter = new JMenuItem("Déconnecter");
-
 		mntmCompteur = new JMenuItem("Compteurs");
 		mntmCharge = new JMenuItem("Charges");
-
 		mntmContratLocation = new JMenuItem("Contrat location");
 		mntmTravaux = new JMenuItem("Travaux");
 		mntmLocataire = new JMenuItem("Locataires");
-
 		mntmHistorique = new JMenuItem("Historique de paiement");
 	}
 
+	/**
+	 * Crée et configure la barre de menu principale de la fenêtre.
+	 * 
+	 * @return la JMenuBar créée et configurée
+	 */
 	protected JMenuBar createHeader() {
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBackground(new Color(214, 214, 214));
@@ -68,6 +63,11 @@ public abstract class FenetreBase extends JFrame {
 		return menuBar;
 	}
 
+	/**
+	 * Crée et configure le panneau de pied de page (footer) de la fenêtre.
+	 *
+	 * @return le JPanel représentant le footer
+	 */
 	protected JPanel createFooter() {
 		JPanel footerPanel = new JPanel();
 		footerPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.LIGHT_GRAY));
@@ -77,5 +77,6 @@ public abstract class FenetreBase extends JFrame {
 		footerPanel.add(footerLabel);
 		return footerPanel;
 	}
+
 
 }
