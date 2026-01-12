0ea89696d0ebe60a43b8d6edfacb34c0792dfb3a
Jay Agil Yoogan <jayagilyoogan@gmail.com>
2026-01-12 10:24:07 +0100

    wer

--- src/controleur/GestionFenetreBienLouable.java
+++ src/controleur/GestionFenetreBienLouable.java
@@ -213,7 +213,7 @@ public class GestionFenetreBienLouable extends GestionHeaderEtFooter implements 
 			List<Locataire> locataires = daoLocataire.findLocataireByBienLouable(idBien);
 			System.out.println(idBien);
 			System.out.println(locataires);
-			if (locataires == null || locataires.isEmpty()){
+			if (locataires == null){
 				JOptionPane.showMessageDialog(
 					    null,
 					    "Ce bien n'a pas de locataire",
